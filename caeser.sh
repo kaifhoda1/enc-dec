32145324dd33cf5e3bae8737c5ae8bcc3df17e7b
CIPHER LEGEND <kaifhoda1@gmail.com>
2026-05-01 18:27:10 +0530

    Create caeser.sh

---
+++ caeser.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "##########@@@@@@@@##########"
+echo "CAESER CIPHER ENCRYPT/DECRYPT"
+echo "##########@@@@@@@@##########"
+echo "PLEASE SELECT 1 TO ENCRYPT AND 2 TO DECRYPT"
+echo "1) ENCRYPTION    2) DECRYPTION"
+echo "PLEASE PROVIDE THE VALUE: "
+read ch
+if [ $ch -eq 1 ]
+then
+        echo "PLEASE PROVIDE THE TEXT TO ENCRYPT: "
+        read enc
+        echo "ENCRYPTED VALUE = $(echo $enc | tr 'a-zA-Z' 'd-za-cD-ZA-C')"
+else
+        echo "PLEASE PROVIDE THE TEXT TO DECRYPT: "
+        read dec
+        echo "DECRYPTED VALUE = $(echo $dec | tr 'd-za-cD-ZA-C' 'a-zA-Z')"
+fi 
