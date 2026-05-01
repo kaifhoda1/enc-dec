03994f059dc7a672f4cceb2d497274f7c567c9f4
CIPHER LEGEND <kaifhoda1@gmail.com>
2026-05-01 18:33:58 +0530

    Fix conditional structure and improve user prompts

--- caeser.sh
+++ caeser.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
+
 echo "##########@@@@@@@@##########"
 echo "CAESER CIPHER ENCRYPT/DECRYPT"
 echo "##########@@@@@@@@##########"
+
 echo "PLEASE SELECT 1 TO ENCRYPT AND 2 TO DECRYPT"
 echo "1) ENCRYPTION    2) DECRYPTION"
 echo "PLEASE PROVIDE THE VALUE: "
 read ch
+
 if [ $ch -eq 1 ]
 then
         echo "PLEASE PROVIDE THE TEXT TO ENCRYPT: "
         read enc
         echo "ENCRYPTED VALUE = $(echo $enc | tr 'a-zA-Z' 'd-za-cD-ZA-C')"
-else
+
+elif [ $ch -eq 2 ]
+then
         echo "PLEASE PROVIDE THE TEXT TO DECRYPT: "
         read dec
         echo "DECRYPTED VALUE = $(echo $dec | tr 'd-za-cD-ZA-C' 'a-zA-Z')"
-fi 
+
+else
+        echo "INVALID OPTION! PLEASE SELECT 1 OR 2 ONLY."
+
+fi
