af61476885a545b99476220c3dd535d9cc1565d7
CIPHER LEGEND <kaifhoda1@gmail.com>
2026-05-02 07:54:53 +0530

    Add ROT13 encryption and decryption script

---
+++ rot13.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo "##########@@@@@@@@##########"
+echo "ROT 13  ENCRYPT/DECRYPT"
+echo "##########@@@@@@@@##########"
+
+echo "PLEASE SELECT 1 TO ENCRYPT AND 2 TO DECRYPT"
+echo "1) ENCRYPTION    2) DECRYPTION"
+echo "PLEASE PROVIDE THE VALUE: "
+read ch
+
+if [ $ch -eq 1 ]
+then
+        echo "PLEASE PROVIDE THE TEXT TO ENCRYPT: "
+        read enc
+        echo "ENCRYPTED VALUE = $(echo $enc | tr 'a-zA-Z' 'n-za-mN-ZA-M')"
+
+elif [ $ch -eq 2 ]
+then
+        echo "PLEASE PROVIDE THE TEXT TO DECRYPT: "
+        read dec
+        echo "DECRYPTED VALUE = $(echo $dec | tr 'a-zA-Z' 'n-za-mN-ZA-M')"
+
+else
+        echo "INVALID OPTION! PLEASE SELECT 1 OR 2 ONLY."
+fi
